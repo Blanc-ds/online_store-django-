305b5e0977069d601dad00e828e5159ce248ed5c
Ilia Blanc <blanc.ilia.il@gmail.com>
2024-09-26 11:04:26 +0200

    added fixtures for 'goods'

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "category"
+
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00 ,max_digits=7, decimal_places=2)
+    discount = models.DecimalField(default=0.00 ,max_digits=4, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(to='Categories', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} quantity - {self.quantity}'
+    
+
+    class Meta:
+        db_table = 'product'
--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
 
 def index(request):
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home',
-        'content': 'Shop\'s main page'
+        'content': 'Shop\'s main page',
+        'categories': categories,
     }
     return render(request, 'main/index.html', context=context)
 
