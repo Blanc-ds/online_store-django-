1947e690b04181b999b9d8234c0a39f5d647969e
Ilia Blanc <blanc.ilia.il@gmail.com>
2024-10-23 19:43:08 +0200

    added new app 'users' + templates

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login(request):
+    context = {
+        'title': 'Home - Login',
+    }
+    return render(request, 'users/login.html', context=context)
+
+def registration(request):
+    context = {
+        'title': 'Home - Registration',
+    }
+    return render(request, 'users/registration.html', context=context)
+
+def profile(request):
+    context = {
+        'title': 'Home - Profile',
+    }
+    return render(request, 'users/profile.html', context=context)
+
+def logout(request):
+    context = {
+        'titile': 'Home - Logout'
+    }
+    pass
